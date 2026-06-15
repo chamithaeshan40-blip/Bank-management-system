2cbc7109c2240c286b195d577f64b44e748f46c8
chamithaeshan40-blip <chamithaeshan40@gmail.com>
2026-06-15 20:14:20 +0900

    add Main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, I'm Chamitha Eshan.");
+        System.out.println("Welcome to my Java Portfolio!");
+
+    }
+}
